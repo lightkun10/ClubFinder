42e1f04af3c042f9718ff4c89c512c8f9be615a0
Fierdy Pandu <32881795+lightkun10@users.noreply.github.com>
2020-11-12 14:19:31 +0700

    Update data-source.js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,22 +1,3 @@
-// Old way, menggunakan constructor function
-// function DataSource(onSuccess, onFailed) {
-//     this.onSuccess = onSuccess;
-//     this.onFailed = onFailed;
-// }
-
-// DataSource.prototype.searchClub = function (keyword) {
-
-//     const filteredClubs = clubs.filter(club => {
-//         return club.name.toUpperCase().includes(keyword.toUpperCase());
-//     });
-
-//     if (filteredClubs.length) {
-//         this.onSuccess(filteredClubs);
-//     } else {
-//         this.onFailed(`${keyword} is not found`);
-//     }
-// };
-
 // Menggunakan Class
 class DataSource {
     
