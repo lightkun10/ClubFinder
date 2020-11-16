8189733e46e02ef8d8736e8c0506b38aa054d1b9
lightkun10 <lifeisubw@gmail.com>
2020-11-16 17:19:56 +0700

    Implement webpack

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import main from './script/view/main.js';
+import './script/component/app-bar.js';
+import './script/component/search-bar.js';
+import "./styles/style.css";
+import "regenerator-runtime";
+
+/**************************
+Entry/main point of app. 
+**************************/
+
+document.addEventListener("DOMContentLoaded", main);
